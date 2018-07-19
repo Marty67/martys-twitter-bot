e1da17aa24ecb9aeacb39a10738ab68c22159bce
Marty67 <Martynas67@gmail.com>
2018-07-19 22:04:13 +0100

    Initial Commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  consumer_key:         'placeholder',
+  consumer_secret:      'placeholder',
+  access_token:         'placeholder',
+  access_token_secret:  'placeholder',
+  //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  //strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
---
+++ index.js
@@ -0,0 +1,42 @@
+console.log('Bot is starting');
+
+var Twit = require('twit');
+var config = require('./config');
+
+//sets up twit and stream with API keys in config
+var T = new Twit(config);
+var stream = T.stream('user');
+
+//sends a tweet
+function tweet(msg){
+  var message = ''+msg;
+  T.post('statuses/update',{status: message}, function(err, data, response) {
+  console.log('-')
+  console.log(data)
+})};
+
+//latest tweet with #banter is sent a tweet
+function tweetOfTheDay(){
+  T.get('search/tweets', { q: '#banter', count: 1 }, function(err, data, response) {
+  var tweets = data.statuses;
+  var target = tweets[0].user.screen_name;
+  tweet('@'+target+' is the banterous lord of the day!');
+})}
+
+//when someone follows, sends a reply and follows them
+function whenFollowed(followEvent){
+  var handle = followEvent.source.screen_name;
+  tweet('@'+handle+' is my new best friend');
+  var target = followEvent.source.id_str;
+  T.post('friendships/create', { id: target }, function(err,data,response){
+    console.log(data)
+  });
+}
+
+tweetOfTheDay();
+
+//sends tweetOfTheDay once a day
+setInterval(tweetOfTheDay,1000*60*60*24);
+
+//when someone follows; execute whenFollowed
+stream.on('follow',whenFollowed);
